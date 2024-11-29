e1c8835c0daa33c6c707039e5e8f46c50bf7b3cc
Orashi6 <bekb0323@gmail.com>
2024-11-29 18:40:18 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+let arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+    11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+
+
+const juft = []
+for (let  i = 0;  i < arr2.length;  i++) {
+    const e = arr2[i];
+
+    if (e % 2) {
+       
+    } else {
+        juft.push(e) 
+    }
+    
+}
+console.log(arr2, 'hamma sonlar');
+console.log(juft, 'juft sonlar');
+
